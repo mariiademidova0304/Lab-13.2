eb1f34c91fa869527b65cd3db42085e4dfdb3f6e
Mariia D <mariia.demidova0304@gmail.com>
2025-08-25 12:58:26 -0400

    Moved mongoose connection to db/

--- server.js
+++ server.js
@@ -3,23 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bookRoutes = require('./routes/bookRoutes');
+const connect = require('./db/connection');
 
-const uri = process.env.MONGO_URI;
+app.use(express.json());
 
+app.use('/api/books', bookRoutes);
 
-mongoose.connect(uri)
-.then(() => {
-    console.log('Successfully connected to MongoDB!')
+connect();
 
-    app.use(express.json());
-
-    app.use('/api/books', bookRoutes);
-
-    const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
     app.listen(port, () => {
         console.log(`Server is running at http://localhost:${port}`);
     })
-})
-.catch(error => console.error('Connection error', error))
+
+
 
 
